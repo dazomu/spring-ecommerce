d815cb80d094d084fd176d3020083f3d38c29bff
Oswaldo Perez <oswaldobell15@gmail.com>
2024-12-27 19:19:41 -0500

    Funcionalidad guardar producto, lista create y logger para la clase ProductoController

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,16 +1,45 @@
 package com.curso.ecommerce.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
 	
-	@GetMapping("") //cadena vacia para que se mapee a producto noma
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
+	@Autowired //el contenedor de spring se encargará de esta variable
+	private ProductoService productoService;
+	
+	@GetMapping("") //cadena vacia para que se mapee a "productos" 
 	public String show(){
 		return "productos/show";
+	}
+	
+	@GetMapping("/create")
+	public String create() {
+		return "productos/create";
+	}
+	
+	
+	@PostMapping("/save") //
+	public String save(Producto producto) {
+		LOGGER.info("Este es el objeto producto {}", producto); //ese objeto lo muestra como toString
+		Usuario u= new Usuario(1, "", "", "", "", "", "", "");
+		producto.setUsuario(u);
+		
+		productoService.save(producto);
+		return "redirect:/productos"; //es una peticion al controlador productos
 	}
 	
 }
