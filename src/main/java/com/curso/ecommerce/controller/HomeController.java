f4dd16027b837f17333e12d4cc14b8b75d2c5d63
Oswaldo Perez <oswaldobell15@gmail.com>
2025-01-05 11:54:09 -0500

    mostrar info del producto de la pagina productohome

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -2,6 +2,8 @@ package com.curso.ecommerce.controller;
 
 
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +13,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.curso.ecommerce.model.Producto;
 import com.curso.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/") //apunta a la raiz
 public class HomeController {
 
-	private final Logger log= LoggerFactory.getLogger(HomeController.class);
+	private final Logger log = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
 	private ProductoService productoService;
+	
+	
+	
+	
 	
 	@GetMapping("")
 	public String home(Model model) {
@@ -32,10 +38,17 @@ public class HomeController {
 		
 	}
 	
-	@GetMapping("/productohome/{id}")
-	public String productoHom(@PathVariable Integer id) {
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
 		
 		log.info("Id enviado como parámetro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get(); 
+		model.addAttribute("producto", producto);
+		
+		System.out.println(id);
+		
 		return "usuario/productohome";
 		
 	//PathVariable hace q la funcion entienda que el id se recibira desde la url	
